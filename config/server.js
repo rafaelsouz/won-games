eb72eeb9b54510b593f0fcd117f6bcee9ad240a8
Rafael Souza <devrafaelsouza@gmail.com>
2021-03-07 20:16:03 -0300

    Iniciando projeto.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1e73bcf25151c21819c44a9dc2c45366'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '23a53531-3187-4c2c-a7a2-b3a8ced9855a'
+};
